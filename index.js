f526f11f99eb8adaae13dc47bd87659a596cb585
jaziel0 <jazielquine@gmail.com>
2024-01-01 19:50:25 -0500

    .

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-
 const container = document.getElementsByClassName("cards")[0]
 
-async function mostrar(){
+async function mostrar() {
+    try {
+        const apiUrl = await fetch('https://rickandmortyapi.com/api/character');
 
-    const apiUrl = await fetch('https://rickandmortyapi.com/api/character');
-    //console.log(apiUrl)
+        const characters = await apiUrl.json();
 
-    const characters = await apiUrl.json();
-    console.log(characters)
-    
-    characters.results.forEach(character => {
-        const card_one = document.getElementsByClassName("card-dad")[0]
-        const name = character.name
-        //console.log(card_one)
-        const nombre_carta = card_one.getElementsByClassName("nombre")
-        //console.log(nombre_carta)
+        characters.results.forEach(character => {
+            const card_one = document.getElementsByClassName("card-dad")[0]
 
-        nombre_carta.innerHTML = name
-        container.appendChild(card_one)
+            const imga = card_one.querySelector(".imga")
+            imga.src = character.image;
 
-        //console.log(container)
-    });
+            const nombre_carta = card_one.querySelector(".nombre");
+            nombre_carta.innerHTML = character.name;
 
-    
+            const status = card_one.querySelector(".status")
+            status.innerHTML = "Status: " + character.status
+
+            const species = card_one.querySelector(".species")
+            species.innerHTML = "Species: " + character.species
+
+            const gender = card_one.querySelector(".gender")
+            gender.innerHTML = character.gender
+
+            // appendChild = agrega nuevos elementos a la pagina
+            const clonedCard = card_one.cloneNode(true);
+            container.appendChild(clonedCard);
+
+        });
+
+    } catch (error) {
+        console.error("Error al obtener datos de la API:", error);
+    }
 }
 
-mostrar()
+mostrar();
